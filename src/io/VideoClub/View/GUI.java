789852b4cb350a1b6f4d860d156ca1d76a74a77d
Manueh <Manueh@DESKTOP-ST5PGCD.home>
2020-02-20 15:21:13 +0100

    Agregación método para validar un nombre

--- src/io/VideoClub/View/GUI.java
+++ src/io/VideoClub/View/GUI.java
@@ -5,12 +5,31 @@
  */
 package io.VideoClub.View;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author Manueh
  */
 public class GUI {
-   public static void principal(){
+    private final static String EXPRESION_NOMBRE="^([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\\']+[\\s])+([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\\'])+"
+            + "[\\s]?([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\\'])?$";
+   
+    
+    public static void principal(){
         
     } 
+   
+    private static boolean validarNombre(String nombre){
+        boolean correcto=false;
+        Pattern pat=Pattern.compile(EXPRESION_NOMBRE);
+        Matcher mat=pat.matcher(nombre);
+
+        if(mat.matches()){
+            correcto=true;
+        }
+
+        return correcto;
+    }
 }

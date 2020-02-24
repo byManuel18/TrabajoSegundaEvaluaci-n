2db269ee22cc9b91241fc3937bf56875f019e66f
alberto <alberto@172.16.15.149>
2020-02-24 14:30:50 +0100

    Creacion de la clase pelicula

---
+++ src/io/VideoClub/Model/Pelicula.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.VideoClub.Model;
+
+import io.VideoClub.Model.Enums.MovieCategory;
+import io.VideoClub.Model.Enums.ProductsTypes;
+
+/**
+ *
+ * @author alberto
+ */
+public class Pelicula extends Product {
+
+    private MovieCategory category;
+
+    public Pelicula(String name, String description, double prize, int edadminima, ProductsTypes tipo, MovieCategory category) {
+         super(name, description, prize, edadminima, tipo);
+        this.category = category;
+    }
+
+    public MovieCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(MovieCategory category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Categoria: " + category + " Edad mínima: " + getEdadmnima();
+    }
+
+}

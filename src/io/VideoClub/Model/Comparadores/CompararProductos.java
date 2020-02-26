93e35bfed82d627508212854f13ffb35756d5e9d
Manueh <Manueh@DESKTOP-ST5PGCD.home>
2020-02-26 20:03:51 +0100

    Terminar comparador productos

--- src/io/VideoClub/Model/Comparadores/CompararProductos.java
+++ src/io/VideoClub/Model/Comparadores/CompararProductos.java
@@ -16,6 +16,10 @@ public class CompararProductos implements Comparator<Product>{
     private enum Criterio{
         AtoZ,
         ZtoA,
+        PorKeyAtoZ,
+        PorkeyZtoA,
+        MenosAMallorPrecio,
+        MallorAMenorPrecio
     }
     private Criterio criterio;
     public CompararProductos() {
@@ -33,9 +37,23 @@ public class CompararProductos implements Comparator<Product>{
         
         switch(this.criterio){
             case AtoZ:
-                
+                resultado=o1.getName().compareTo(o2.getName());
                 break;
-            default:
+            case ZtoA:
+                resultado=o2.getName().compareTo(o1.getName());
+                break;
+            case PorKeyAtoZ:
+                resultado=o1.getKey().compareTo(o2.getKey());
+                break;  
+            case PorkeyZtoA:
+                resultado=o2.getKey().compareTo(o1.getKey());
+                break;
+            case MenosAMallorPrecio:
+                resultado=(int)(o1.getPrize()-o2.getPrize());
+                break;
+            case MallorAMenorPrecio:
+                 resultado=(int)(o2.getPrize()-o1.getPrize());
+                break;
                 
         }
         

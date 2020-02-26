5605b9ed70843fed6ddc1a9602cda971f1dd2ada
Manueh <Manueh@DESKTOP-ST5PGCD.home>
2020-02-26 18:07:54 +0100

    Creacion comparar

---
+++ src/io/VideoClub/Model/Comparadores/CompararaClientes.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.VideoClub.Model.Comparadores;
+
+import io.VideoClub.Model.Client;
+
+/**
+ *
+ * @author Manueh
+ */
+public class CompararaClientes implements Comparable<Client>{
+
+    @Override
+    public int compareTo(Client o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

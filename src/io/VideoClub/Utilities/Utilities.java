c7bb9c3dd7beda1892eac2157cfad76e8bd5eb52
Manueh <Manueh@DESKTOP-ST5PGCD.home>
2020-03-09 18:08:24 +0100

    primera parte del menú

--- src/io/VideoClub/Utilities/Utilities.java
+++ src/io/VideoClub/Utilities/Utilities.java
@@ -14,42 +14,50 @@ import java.util.regex.Pattern;
  * @author Manueh
  */
 public class Utilities {
+
     private static Scanner keyboard = new Scanner(System.in);
-    private final static String EXPRESION_NOMBRE="^([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\\']+[\\s])+([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\\'])+"
+    private final static String EXPRESION_NOMBRE = "^([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\\']+[\\s])+([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\\'])+"
             + "[\\s]?([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\\'])?$";
-    
+
     /**
      * Método que valida un nombre
+     *
      * @param nombre el nombre a validar
      * @return devulve true si es un nombre válido y false si no lo es
      */
-    private static boolean validarNombre(String nombre){
-        boolean correcto=false;
-        Pattern pat=Pattern.compile(EXPRESION_NOMBRE);
-        Matcher mat=pat.matcher(nombre);
+    private static boolean validarNombre(String nombre) {
+        boolean correcto = false;
+        Pattern pat = Pattern.compile(EXPRESION_NOMBRE);
+        Matcher mat = pat.matcher(nombre);
 
-        if(mat.matches()){
-            correcto=true;
+        if (mat.matches()) {
+            correcto = true;
         }
 
         return correcto;
     }
+
     /**
      * Escribe un texto en consola sin retorno de carro
+     *
      * @param f texto a imprimir
      */
     public static void p(String f) {
         System.out.print(f);
     }
+
     /**
      * Escribe un texto en consola con retorno de carro
+     *
      * @param f texto a imprimir
      */
     public static void P(String f) {
         Utilities.p(f + "\n");
     }
+
     /**
      * Lee un entero de teclado
+     *
      * @return devuelve el entero leído
      */
     public static int getInt() {
@@ -65,15 +73,17 @@ public class Utilities {
                 Utilities.P("Error in keyboard. Please, try it again: ");
             } catch (NumberFormatException ex) {
                 Utilities.P("Error reading integer type. Please, try it again: ");
-            }catch (Exception ex) {
+            } catch (Exception ex) {
                 ex.printStackTrace();
                 Utilities.P("Error unknown. Please, try it again: ");
             }
         } while (!valid);
         return result;
     }
+
     /**
      * Lee un entero de teclado
+     *
      * @param f Mensaje a imprimir al usuario antes de solicitar el entero
      * @return devuelve el entero leído
      */
@@ -81,8 +91,10 @@ public class Utilities {
         Utilities.p(f + " : ");
         return Utilities.getInt();
     }
+
     /**
      * Lee un float de teclado
+     *
      * @return devuelve el float leído
      */
     public static double getDouble() {
@@ -100,12 +112,14 @@ public class Utilities {
                 Utilities.P("Error reading decimal number. Please, try it again: ");
             } catch (Exception ex) {
                 Utilities.P("Error unknown. Please, try it again: ");
-            } 
+            }
         } while (!valid);
         return result;
     }
+
     /**
      * Lee un float del teclado, imprimiendo previamente un mensaje al usuario
+     *
      * @param f mensaje a imprimir antes de solicitar el float
      * @return float insertado por el usuario
      */
@@ -113,8 +127,10 @@ public class Utilities {
         Utilities.p(f + " : ");
         return Utilities.getDouble();
     }
+
     /**
      * Lee un string de teclado
+     *
      * @return strint insertado por el usuario
      */
     public static String getString() {
@@ -122,7 +138,7 @@ public class Utilities {
         boolean valid = false;
         do {
             try {
-                result = keyboard.nextLine();
+                result = keyboard.nextLine().toUpperCase();
                 valid = true;
 
             } catch (Exception ex) {
@@ -131,8 +147,10 @@ public class Utilities {
         } while (!valid);
         return result;
     }
+
     /**
      * Lee un string de teclado, imprimiendo previamente un mensaje
+     *
      * @param f mensaje a mostrar antes de solicitar el string
      * @return string insertado por el usuario
      */
@@ -140,40 +158,105 @@ public class Utilities {
         Utilities.p(f + " : ");
         return Utilities.getString();
     }
-   public static int Menu(){
+
+    public static int Menu() {
         int opcion;
-        
+
         P("-----Bienvenido al videoclub-----");
         P("1)Listar productos");
-        P("2)Listar productos por tipo");
-        P("3)Listar los productos ordenados");
-        P("4)Listar productos por nombre");
-        P("5)Listar productos por nombre y tipo");
-        P("6)Listar los productos por estado");
-        P("7)Listar diferentes productos");
-        P("8)Listar peliculas");
-        P("9)Listar juegos");
-        P("10)Listar catidad de productos por nombre");
-        P("11)Listar catidad de productos por nombre y tipo");
-        P("12)Mostrar clientes");
-        P("13)Mostrar todos los clientes(comparados)");
-        P("14)Mostrar clientes cuya reserva no ha acabado");
-        P("15)Mostrar todas las reservas ");
-        P("16)Listar las reservas (ordenadas)");
-        P("17)Listar reservas por estado");
-        P("18)Crear Producto");
-        P("19)Añadir cliente");
-        P("20)Borrar cliente");
-        P("21)Añadir producto");
-        P("22)Borrar producto");
-        P("23)Editar producto");
-        P("24)Consultar productos disponibles");
-        P("25)Cerrar reserva");
-        P("26)Salir");
+        P("2)Mostrar clientes");
+        P("3)Listar reservas");
+        P("4)Añadir cliente");
+        P("5)Crear producto");
+        P("6)Añadir existencia de un producto");
+        P("7)Editar producto");
+        P("8)Borrar producto");
+        P("9)Consultar productos disponibles");
+        P("10)Añadir cliente");
+        P("11)Borrar cliente");
+        P("12)Editar cliente");
+        P("13)Hacer reserva");
+        P("14)Cerrar reserva");
+        P("15)Salir");
         P("-------------------------------------");
-        
-        opcion=getInt(); //Llama a la funcion LeerNUmeros para darle un valor por teclado a opción
-        
+        p("> ");
+
+        opcion = getInt(); //Llama a la funcion LeerNUmeros para darle un valor por teclado a opción
+
         return opcion;
     }
+
+    public static int MenuListarProductor() {
+        int resultado = 0;
+
+        P("1)Listar todos");
+        P("2)Listar Productos ordenados");
+        P("3)Listar por nombre");
+        P("4)Listar pot tipo y nombre");
+        P("5)Listar por tipo");
+        P("6)Listar por estado");
+        P("7)Listar películas");
+        P("8)Listar juegos");
+        P("9)Listar catidad de productos por nombre");
+        P("10)Listar catidad de productos por nombre y tipo");
+        P("11)Volver al menú anterior");
+        p("> ");
+        resultado = getInt();
+
+        return resultado;
+    }
+
+    public static int MenuListarClientes() {
+        int resultado = 0;
+        
+        P("1)Listar todos los clientes");
+        P("2)Mostrar los clientes ordenados");
+        P("3)Mostrar los clientes cuya reserva no ha finalizado");
+        p("> ");
+        resultado = getInt();
+
+        return resultado;
+
+    }
+    
+    public static int MenuListarreservas() {
+        int resultado = 0;
+        
+        P("1)Listar todas las reservas");
+        P("2)Mostrar las reservas ordenadas");
+        P("3)Mostrar las reservas por estado");
+        p("> ");
+        resultado = getInt();
+
+        return resultado;
+
+    }
+    
+    public static int MenuOrdenarProductos(){
+        int resultado = 0;
+        
+        P("1)De la A a la Z");
+        P("2)De la Z a la A");
+        P("3)De Mayor a Menor precio");
+        P("4)De Menor a Meyor precio");
+        P("5)Por key de la A a la Z");
+        P("6)Por key de la Z a la A");
+        P("7)Volver al menú anterior");
+        p("> ");
+        resultado = getInt();
+
+        return resultado;
+    }
+    
+    public static int MenuTipoProducto(){
+        int resultado=0;
+        P("Introduce el tipo de Producto:");
+        P("1)Juego");
+        P("2)Otro");
+        P("3)Película");
+        p("> ");
+        resultado=getInt();
+        return resultado;
+    }
+
 }
--- src/io/VideoClub/View/GUI.java
+++ src/io/VideoClub/View/GUI.java
@@ -7,6 +7,7 @@ package io.VideoClub.View;
 
 import io.VideoClub.Controller.AppController;
 import io.VideoClub.Model.Client;
+import io.VideoClub.Model.Comparadores.CompararProductos;
 import io.VideoClub.Model.Comparadores.CompararaClientes;
 import io.VideoClub.Model.Enums.GameCategory;
 import io.VideoClub.Model.Enums.MovieCategory;
@@ -14,15 +15,16 @@ import io.VideoClub.Model.Enums.ProductsTypes;
 import io.VideoClub.Model.Product;
 import io.VideoClub.Utilities.Utilities;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 /**
  *
  * @author Manueh
  */
 public class GUI {
-
+static AppController ni=AppController.getInstance();
     public static void principal() {
-        AppController ni = AppController.getInstance();
+        
         //System.out.println(ni.createClient("rqwe", "fasef", "ad", LocalDateTime.now()));
         /*
         System.out.println(ni.listAllProducts());
@@ -37,9 +39,9 @@ public class GUI {
         //System.out.println(ni.listAllAmountOfProducts("Minecraft"));
         //System.out.println("---------------");
         //System.out.println(ni.addProduct("300", ProductsTypes.Peliculas));
-        System.out.println(ni.listAllProducts());
-        System.out.println(ni.listAllReservations());
-        System.out.println(ni.saveAllDDBB());
+       // System.out.println(ni.listAllProducts());
+        //System.out.println(ni.listAllReservations());
+        //System.out.println(ni.saveAllDDBB());
         
         //System.out.println(ni.listAllAmountOfProducts("Minecraft"));
         /* 
@@ -47,7 +49,7 @@ public class GUI {
         System.out.println(ni.loadCatalogFromDDBB());
         System.out.println(ni.listAllProducts());
          */
-        // PrimerMenu();
+        PrimerMenu();
     }
 
     private static void PrimerMenu() {
@@ -56,13 +58,21 @@ public class GUI {
         do {
             opcion = Utilities.Menu();
             ControladorPrimerMenu(opcion);
-        } while (opcion != 26);
+        } while (opcion != 15);
 
     }
 
     private static void ControladorPrimerMenu(int op1) {
+        int opcion2=0;
         switch (op1) {
             case 1:
+                do{
+                   
+                    opcion2=Utilities.MenuListarProductor();
+                    ControladorMenuProductos(opcion2);
+                    
+                }while(opcion2!=11);
+                
                 break;
             case 2:
                 break;
@@ -74,9 +84,171 @@ public class GUI {
                 break;
             case 6:
                 break;
+                
+            case 15:
+                
+                break;
             default:
                 Utilities.P("Opción no válida, vuelve a intentarlo.");
         }
+    }
+    
+    private static void ControladorMenuProductos(int op2){
+        Set<Product> pro;
+        String nombre;
+        ProductsTypes tipo=null;
+        int opListaPro=0;
+        switch(op2){
+            case 1:
+                pro=ni.listAllProducts();
+                if(!pro.isEmpty()){
+                    ListarSetProductos(pro);
+                }else{
+                    Utilities.P("No existen Productos aún.");
+                }
+                break;
+                
+            case 2:
+                
+                do{
+                    opListaPro=Utilities.MenuOrdenarProductos();
+                    MostrarProductosOrdenados(opListaPro);
+                }while(opListaPro!=7);
+               
+                break;
+            case 3:
+                nombre=Utilities.getString("Introduce el nombre del producto");
+                pro=ni.listAllByName(nombre);
+                if(!pro.isEmpty()){
+                    for(Product p:pro){
+                       Utilities.P(p.toString());
+                    }
+                }else{
+                    Utilities.P("No existen com esa especificación aún.");
+                }
+                break;
+            case 4:
+                nombre=Utilities.getString("Introduce el nombre del producto");
+                do{
+                    opListaPro=Utilities.MenuTipoProducto();
+                    tipo=devolverTypoProducto(opListaPro);
+                }while(tipo==null);
+                
+                pro=ni.listAllByName(nombre, tipo);
+                if(!pro.isEmpty()){
+                    ListarSetProductos(pro);
+                }else{
+                    Utilities.P("Lista vacía.");
+                }
+                 
+                break;
+            case 5:
+                do{
+                    opListaPro=Utilities.MenuTipoProducto();
+                    tipo=devolverTypoProducto(opListaPro);
+                }while(tipo==null);
+                pro=ni.listAllByType(tipo);
+                if(!pro.isEmpty()){
+                    ListarSetProductos(pro);
+                }else{
+                    Utilities.P("Lista vacía.");
+                }
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            case 11:
+                break;
+            default:
+                Utilities.P("Opción no válida, vuelve a intentarlo.");
+        }
+    }
+    
+    private static void MostrarProductosOrdenados(int op){
+        CompararProductos.Criterio comp=null;
+        switch(op){
+            case 1:
+                comp=CompararProductos.Criterio.AtoZ;
+                ListarProductosOrdenados(comp);
+                break;
+            case 2:
+                comp=CompararProductos.Criterio.ZtoA;
+                ListarProductosOrdenados(comp);
+                break;
+            case 3:
+                comp=CompararProductos.Criterio.MallorAMenorPrecio;
+                ListarProductosOrdenados(comp);
+                break;
+            case 4:
+                comp=CompararProductos.Criterio.MenosAMallorPrecio;
+                ListarProductosOrdenados(comp);
+                break;
+            case 5:
+                comp=CompararProductos.Criterio.PorKeyAtoZ;
+                ListarProductosOrdenados(comp);
+                break;
+            case 6:
+                comp=CompararProductos.Criterio.PorkeyZtoA;
+                ListarProductosOrdenados(comp);
+                break;
+            case 7:
+                Utilities.P("Volviendo al menú anterior.");
+                break;
+            default:
+                Utilities.P("Opción no válida, vuelve a intentarlo.");
+   
+        }
+        
+        
+        
+    }
+    
+    private static void ListarProductosOrdenados(CompararProductos.Criterio c){
+        CompararProductos comparador=new CompararProductos(c);
+        Set<Product> pro=ni.listAllProducts(comparador);
+        if(!pro.isEmpty()){
+            for(Product p:pro){
+                Utilities.P(p.toString());
+            }
+        }else{
+             Utilities.P("No existen Productos aún.");
+        }
+    }
+    
+    private static ProductsTypes devolverTypoProducto(int op){
+            ProductsTypes tipo=null;
+            switch(op){
+                case 1:
+                    tipo=ProductsTypes.Juegos;
+                    break;
+                case 2:
+                    tipo=ProductsTypes.Otros;
+                    break;
+                case 3: 
+                    tipo=ProductsTypes.Peliculas;
+                    break;
+                default:
+                    Utilities.P("Opción incorrecta. Prueba de nuevo.");
+            }
+            return tipo;
+        }
+    private static void ListarSetProductos(Set<Product> listap){
+        
+        if(!listap.isEmpty()){
+            for(Product p:listap){
+                Utilities.P(p.toString());
+            }
+        }else{
+            Utilities.P("Lista vacía.");
+        }
+        
     }
 
 }
